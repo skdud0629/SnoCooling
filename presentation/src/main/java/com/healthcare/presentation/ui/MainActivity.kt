acebcf2fdab3ff283fc636bb92ff38032b01fc40
Thirfir <sangbbang31@gmail.com>
2023-11-17 10:51:43 +0900

    [add] Request initial permissions

--- presentation/src/main/java/com/healthcare/presentation/ui/MainActivity.kt
+++ presentation/src/main/java/com/healthcare/presentation/ui/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.healthcare.presentation.ui
 
+import android.Manifest
+import android.os.Build
+import androidx.fragment.app.Fragment
 import com.healthcare.presentation.R
 import com.healthcare.presentation.databinding.ActivityMainBinding
 import com.healthcare.presentation.ui.analysis.AnalysisFragment
@@ -22,23 +25,44 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
     }
 
     override fun initEvent() {
+        requestRequiredPermissions()
+        initBottomNavigationEvent()
+    }
+
+    private fun initBottomNavigationEvent() {
         binding.bottomNavigation.setOnItemSelectedListener {
-            val fm = supportFragmentManager
             when(it.itemId) {
                 R.id.navigation_item_home -> {
-                    fm.beginTransaction()
-                        .replace(R.id.main_fragment_container, TrackingFragment())
-                        .commit()
+                    replaceFragment(TrackingFragment())
                     true
                 }
                 R.id.navigation_item_analysis -> {
-                    fm.beginTransaction()
-                        .replace(R.id.main_fragment_container, AnalysisFragment())
-                        .commit()
+                    replaceFragment(AnalysisFragment())
                     true
                 }
                 else -> false
             }
         }
+    }
+
+    private fun requestRequiredPermissions() {
+        val permissions = arrayListOf(
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.RECORD_AUDIO
+        )
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            permissions.add(Manifest.permission.READ_MEDIA_AUDIO)
+            permissions.add(Manifest.permission.POST_NOTIFICATIONS)
+        }
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
+            permissions.add(Manifest.permission.READ_EXTERNAL_STORAGE)
+        }
+        requestPermissions(permissions.toTypedArray(), 0)
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fm = supportFragmentManager
+        fm.beginTransaction().replace(R.id.main_fragment_container, fragment).commit()
     }
 }
