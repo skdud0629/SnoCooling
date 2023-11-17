bf231fbf9974fb3a20cffacdb62000183f092a31
Strone <sangbbang31@gmail.com>
2023-11-17 12:28:14 +0900

    [add] Initial Tracking Service

---
+++ presentation/src/main/java/com/healthcare/presentation/service/TrackingService.kt
@@ -0,0 +1,12 @@
+package com.healthcare.presentation.service
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class TrackingService : Service() {
+
+    override fun onBind(intent: Intent): IBinder? {
+        return null
+    }
+}
--- presentation/src/main/java/com/healthcare/presentation/ui/tracking/TrackingFragment.kt
+++ presentation/src/main/java/com/healthcare/presentation/ui/tracking/TrackingFragment.kt
@@ -1,8 +1,11 @@
 package com.healthcare.presentation.ui.tracking
 
+import android.content.Intent
+import android.os.Build
 import androidx.fragment.app.viewModels
 import com.healthcare.presentation.R
 import com.healthcare.presentation.databinding.FragmentTrackingBinding
+import com.healthcare.presentation.service.TrackingService
 import com.healthcare.presentation.ui.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -11,12 +14,21 @@ class TrackingFragment : BaseFragment<FragmentTrackingBinding>() {
     override val layoutId: Int
         get() = R.layout.fragment_tracking
 
+    private val trackingIntent by lazy {
+        Intent(requireContext(), TrackingService::class.java)
+    }
+
     override fun initView() {
 
     }
 
     override fun initEvent() {
-
+        binding.buttonStartTracking.setOnClickListener {
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+                requireContext().startForegroundService(trackingIntent)
+            else
+                requireContext().startService(trackingIntent)
+        }
     }
 
     override fun subscribe() {
